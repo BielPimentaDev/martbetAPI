b1e12252e7023bca109a02f9eb64b5f78f851a0d
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2023-04-19 19:11:59 -0300

    changing the agent instance

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const config = {
 	headers: {
-		cookie:
-			'JSESSIONID=733A22D1AE2018E0B9A5B21C1F450DBC; __cf_bm=7HkHlY0rGhSBu.NY.mBScCjh4QtNenOHVu66wdU2atY-1676339383-0-AYCAW0Z4jIuoR7CtjJrJLQlT8aoo6WRUR1bpSpLibud0pbRgcorNhX9K1%2FW%2BqiCf%2F8WTX81CtQzLceDkwn%2Fcgek%3D',
+		// cookie:
+		// 	'JSESSIONID=733A22D1AE2018E0B9A5B21C1F450DBC; __cf_bm=7HkHlY0rGhSBu.NY.mBScCjh4QtNenOHVu66wdU2atY-1676339383-0-AYCAW0Z4jIuoR7CtjJrJLQlT8aoo6WRUR1bpSpLibud0pbRgcorNhX9K1%2FW%2BqiCf%2F8WTX81CtQzLceDkwn%2Fcgek%3D',
 		gamexAPIAgent: 'JozelioMv.com.GamesApp.1.0',
-		gamexAPIAgentInstance: '18fa9da939a39dbad44a70b2560435c4',
+		gamexAPIAgentInstance: '99da719686def45bf417c195bf3156f7',
 		gamexAPIPassword: 'Joze852#',
 		'Content-Type': 'application/xml',
 	},
