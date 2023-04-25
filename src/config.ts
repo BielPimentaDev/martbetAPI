4419597b672569aeb9ddd1dc1e49a19a8cbf0190
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2023-04-25 13:18:15 -0300

    changing user agent

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const config = {
 		// cookie:
 		// 	'JSESSIONID=733A22D1AE2018E0B9A5B21C1F450DBC; __cf_bm=7HkHlY0rGhSBu.NY.mBScCjh4QtNenOHVu66wdU2atY-1676339383-0-AYCAW0Z4jIuoR7CtjJrJLQlT8aoo6WRUR1bpSpLibud0pbRgcorNhX9K1%2FW%2BqiCf%2F8WTX81CtQzLceDkwn%2Fcgek%3D',
 		gamexAPIAgent: 'JozelioMv.com.GamesApp.1.0',
-		gamexAPIAgentInstance: '99da719686def45bf417c195bf3156f7',
+		gamexAPIAgentInstance: '44f9d71dee78810c98aa7c425d8291cb',
 		gamexAPIPassword: 'Joze852#',
 		'Content-Type': 'application/xml',
 	},
